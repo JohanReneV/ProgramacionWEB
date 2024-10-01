167c9e304e0f9c606d7963b4c35eb0dd20d4f1f7
JohanReneV <momonga12@outlook.es>
2024-10-01 16:20:02 -0500

    Class 1-10

---
+++ Clas_26_09/main.js
@@ -0,0 +1,13 @@
+
+alert("Hola mundo");
+
+console.log("Hola mundo");
+
+
+let variable=prompt("ingrese un dato");
+console.log("Ingrese un dato" + variable);
+
+
+let a = Number(prompt("Ingrese A"));
+let b = Number(prompt("Ingrese B"));
+console.log("El resultado es: " + (a+b));
